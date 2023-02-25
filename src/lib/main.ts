ac7efc0f9da20bc85f7bf88c5070843a16f51ce9
Alex Long <alex@feeditback.com>
2023-02-25 22:43:36 +0000

    updated config

---
+++ src/lib/main.ts
@@ -0,0 +1,8 @@
+import XfButton from "@/components/XfButton/XfButton.vue";
+import XfTextInput from "@/components/inputs/XfTextInput/XfTextInput.vue";
+import XfTextArea from "@/components/inputs/XfTextArea/XfTextArea.vue";
+import XfIcon from "@/components/XfIcon/XfIcon.vue";
+import XfModal from "@/components/XfModal/XfModal.vue";
+import XfBanner from "@/components/XfBanner/XfBanner.vue";
+
+export { XfBanner, XfButton, XfIcon, XfModal, XfTextArea, XfTextInput };
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, resolve, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -16,6 +16,21 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "/src/lib/main.ts"),
+      name: "xFCmptLib",
+      fileName: "xf-cmpt-lib",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
 });
